922d4f09e8f35920d896c6ffb73f71b21fc4e6ef
ruslanmaherramov <rmaherramov@gmail.com>
2024-07-29 00:30:01 +0300

    add exercise 1

---
+++ src/components/Form/Form.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+function Form() {
+  const [guess, setGuess] = useState('');
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+
+      console.log(guess);
+      
+      setGuess('');
+  }
+
+  return (
+      <form
+          className="guess-input-wrapper"
+          onSubmit={handleSubmit}
+      >
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+            required
+            type="text"
+            id="guess-input"
+            pattern="[a-zA-Z]{5}"
+            title="5 letter word"
+            maxLength={5}
+            minLength={5}
+            value={guess}
+            onChange={(event) => {
+              const nextGuess = event.target.value.toUpperCase();
+              setGuess(nextGuess);
+            }}
+        />
+      </form>
+  );
+}
+
+export default Form;
